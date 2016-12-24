8b35617f145602d95c649b45e2d7ae9159cae21e
calebcartwright <caleb.cartwright@outlook.com>
2016-12-24 17:43:10 -0500

    Creating initial projects, and then calling it a day for xmas eve

---
+++ Cmc.Fourier.Algorithms.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cmc.Fourier.Algorithms.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
